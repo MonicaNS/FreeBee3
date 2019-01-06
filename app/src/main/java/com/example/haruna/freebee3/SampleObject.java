1ad6954ae22a5dcf3e617716860cdcf64fd15d6c
MockingJ <Monica.Saghar@gmail.com>
2019-01-06 00:56:03 -0800

    I added 3 new classes but crashes right now. Will be fixed soon

---
+++ app/src/main/java/com/example/haruna/freebee3/SampleObject.java
@@ -0,0 +1,161 @@
+package com.example.haruna.freebee3;
+
+public class SampleObject {
+
+    /*String header1;
+    String header2;
+    String header3;
+    String header4;
+    String header5;
+    String header6;
+    String header7;
+    String header8;
+    String header9, h10,h11, h12,h13,h14,h15,h16,h17,h18,h19,h20,
+    h21,h22,h23,h24,h25,h26,h27,h28,h29,h30,h31,h32,h33,h34,h35,
+    h36,h37,h38,h39,h40,h41,h42,h43,h44,h45,h46,h47,h48;*/
+    String [] array;
+
+    public SampleObject(String header1, String header2, String header3,
+                        String header4, String header5, String header6,
+                        String header7, String header8, String header9,
+                        String h10, String h11, String h12, String h13,
+                        String h14, String h15, String h16, String h17,
+                        String h18, String h19, String h20, String h21,
+                        String h22, String h23, String h24, String h25,
+                        String h26, String h27, String h28, String h29,
+                        String h30, String h31, String h32, String h33,
+                        String h34, String h35, String h36, String h37,
+                        String h38, String h39, String h40, String h41,
+                        String h42, String h43, String h44,  String h45,
+                        String h46, String h47,  String h48){
+
+        array = new String[47];
+        array[0] = header1;
+        array[1] = header2;
+        array[2] = header3;
+        array[3] = header4;
+        array[4] = header5;
+        array[5] = header6;
+        array[6] = header7;
+        array[7] = header8;
+        array[8] = header9;
+        array[9] = h10;
+        array[10] = h11;
+        array[11] = h12;
+        array[12] = h13;
+        array[13] = h14;
+        array[14] = h15;
+        array[15] = h16;
+        array[16] = h17;
+        array[17] = h18;
+        array[18] = h19;
+        array[19] = h20;
+        array[20] = h21;
+        array[21] = h22;
+        array[22] = h23;
+        array[23] = h24;
+        array[24] = h25;
+        array[25] = h26;
+        array[26] = h27;
+        array[27] = h28;
+        array[28] = h29;
+        array[29] = h30;
+        array[30] = h31;
+        array[31] = h32;
+        array[32] = h33;
+        array[33] = h34;
+        array[34] = h35;
+        array[35] = h36;
+        array[36] = h37;
+        array[37] = h38;
+        array[38] = h39;
+        array[39] = h40;
+        array[40] = h41;
+        array[41] = h42;
+        array[42] = h43;
+        array[43] = h44;
+        array[44] = h45;
+        array[45] = h46;
+        array[46] = h47;
+        array[47] = h48;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*this.header1 = header1;
+        this.header2 = header2;
+        this.header3 = header3;
+        this.header4 = header4;
+        this.header5 = header5;
+        this.header6 = header6;
+        this.header7 = header7;
+        this.header8 = header8;
+        this.header9 = header9;
+        this.h10 = h10;
+        this.h11 = h11;
+        this.h12 = h12;
+        this.h13 = h13;
+        this.h14 = h14;
+
+        this.h15 = h15;
+        this.h16 = h16;
+        this.h17 = h17;
+        this.h18 = h18;
+        this.h19 = h19;
+        this.h20 = h20;
+        this.h21 = h21;
+        this.h22 = h22;
+        this.h23 = h23;
+        this.h24 = h24;
+        this.h25 = h25;
+        this.h26 = h26;
+        this.h27 = h27;
+        this.h28 = h28;
+        this.h29 = h29;
+        this.h30 = h30;
+        this.h31 = h31;
+        this.h32 = h32;
+        this.h33 = h33;
+        this.h34 = h34;
+        this.h35 = h35;
+        this.h36 = h36;
+        this.h37 = h37;
+        this.h38 = h38;
+        this.h39 = h39;
+        this.h40 = h40;
+        this.h41 = h41;
+        this.h42 = h42;
+        this.h43 = h43;
+        this.h44 = h44;
+        this.h45 = h45;
+        this.h46 = h46;
+        this.h47 = h47;
+        this.h48 = h48;
+
+        String str[] = {h1, h2};*/
+
+    }
+
+    public String getHeader( int headerNumber) {
+       return array[headerNumber];
+    }
+};
